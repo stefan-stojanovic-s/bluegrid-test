6495fdce6086e5c95816ca03a0cff64d72169769
stefan <stefanstojanovicbusiness73@gmail.com>
2020-12-10 08:26:56 +0100

    added ses so it sends a message if injecting was success or not

--- lambda/lambda-function.py
+++ lambda/lambda-function.py
@@ -3,6 +3,34 @@ import json
 import boto3
 import time
 
+
+def send_email(email_data):
+    client = boto3.client("ses")
+    response = client.send_email(
+    Destination={
+        'BccAddresses': [
+        ],
+        'CcAddresses': [
+        ],
+        'ToAddresses': [
+            'stefanstojanovicbusiness73@gmail.com',
+        ],
+    },
+    Message={
+        'Body': {
+            'Text': {
+                'Charset': 'UTF-8',
+                'Data': f'Injected message ==> {email_data["message"]}' if email_data["message"] else "Error while injecting the message",
+            },
+        },
+        'Subject': {
+            'Charset': 'UTF-8',
+            'Data': email_data["subject"],
+        },
+    },
+    Source='stefanstojanovicbusiness73@gmail.com')
+
+
 def process_dynamodb_response(response):
     #We can see the response from dynamoDB in test events
     message = None
@@ -30,8 +58,7 @@ def lambda_handler(event, context):
         
         #getting the first running instance
         instance_id = get_active_instance(describe_instances)
-        print(instance_id)
-        
+
         #we send argument => message in python execute command so it gets injected to an html file 
         #via python script on EC2 instance
         #https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ssm.html#SSM.Client.send_command
@@ -43,18 +70,21 @@ def lambda_handler(event, context):
                 )
     
     
-        #the chunk of code bellow is redundant for this use case
-        #we might use it if we want to handle notiications for message success usins SES
         
-        # command_id = response['Command']['CommandId']
-        # #waiting for command to finish on ssm, there is also waiter boto3 methods for this
-        # time.sleep(4)
+        command_id = response['Command']['CommandId']
+        #waiting for command to finish on ssm, there is also waiter boto3 methods for this
+        #so we can get the command invocation; we can use the output of the command to determine
+        #if the injection was succesfull
+        time.sleep(4)
         
-        # #https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ssm.html#SSM.Client.get_command_invocation        
-        # output = ssm.get_command_invocation(
-        #       CommandId=command_id,
-        #       InstanceId=instance_id)
+        #https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ssm.html#SSM.Client.get_command_invocation        
+        output = ssm.get_command_invocation(
+              CommandId=command_id,
+              InstanceId=instance_id)
     
-        # print(output)
+        #send the outcome of the script to the email using ses, 
+        #it felt as it was the next logical step of automating this task
+        returned_output = json.loads(output["StandardOutputContent"])
+        send_email(returned_output)
         
 
